f768ba526a663cc94eefef4cc88bcccdede6f319
Wkinfei <kinfeiwong92@gmail.com>
2023-03-28 12:09:05 +0800

    first

---
+++ server-37/src/main/java/iss/nus/server37/Server37Application.java
@@ -0,0 +1,13 @@
+package iss.nus.server37;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server37Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Server37Application.class, args);
+	}
+
+}
---
+++ server-37/src/test/java/iss/nus/server37/Server37ApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.nus.server37;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server37ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
